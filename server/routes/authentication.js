3c6b47e6a7955f2dc32575fecdf15e129855f9af
naimulh247 <naimulh247@gmail.com>
2021-11-27 06:55:06 +0000

    save user to mongodb on signup

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-
+const mongoose = require('mongoose')
+const User = mongoose.model("User")
 
 
 router.get('/', (req, res)=>{
@@ -14,7 +15,30 @@ router.post('/signup', (req, res)=>{
     if(!email || !password){
         return res.status(400).json({err: "Missing fields"})
     }
-    res.json({message:"Succesfully sent account info"})
+
+    User.findOne({email}).then((existingUser) =>{
+        if(existingUser){
+            return res.status(422).json({err:"User alreay exists"})
+        }
+
+        const newUser = new User({
+            email,
+            password
+        })
+
+        newUser.save()
+        .then(newUser =>{
+            res.json({message:"Created user"})
+        })
+        .catch (err=>{
+            console.log(err)
+        })
+    })
+    .catch(error=>{
+        console.log(error)
+    })
+    // res.json({message:"Succesfully sent account info"})
+    
 })
 
 module.exports = router

0c512d3617f3946489b126c0e8c302f15b409b5b
naimulh247 <naimulh247@gmail.com>
2021-11-27 06:59:36 +0000

    hash signup password

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -1,12 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 const User = mongoose.model("User")
 
-
-router.get('/', (req, res)=>{
-    res.send("hellow")
-})
 
 router.post('/signup', (req, res)=>{
     console.log(req.body.email)
@@ -21,23 +18,26 @@ router.post('/signup', (req, res)=>{
             return res.status(422).json({err:"User alreay exists"})
         }
 
-        const newUser = new User({
-            email,
-            password
+        bcrypt.hash(password, 14).then(hashedPassword =>{
+            const newUser = new User({
+                email,
+                password:hashedPassword
+            })
+    
+            newUser.save()
+            .then(newUser =>{
+                res.json({message:"Created user"})
+            })
+            .catch (err=>{
+                console.log(err)
+            })
         })
+        .catch(error=>{
+            console.log(error)
+        })
+        })
+        
 
-        newUser.save()
-        .then(newUser =>{
-            res.json({message:"Created user"})
-        })
-        .catch (err=>{
-            console.log(err)
-        })
-    })
-    .catch(error=>{
-        console.log(error)
-    })
-    // res.json({message:"Succesfully sent account info"})
     
 })
 
